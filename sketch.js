b9d83440fbd4a460e249199b116a7c9c451aa592
coderHarsh-cloud <68999280+coderHarsh-cloud@users.noreply.github.com>
2020-08-05 13:32:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,148 +1,250 @@
-var monkey,monkeyImage,stone,stoneImage,banana,bananaImage,foodGroup,stoneGrouup,jungle,jungleImage,gameState,ground,groundImage,
+//declaring all the variables
+var monkey,monkeyImage,stone,stoneImage,banana,bananaImage,foodGroup,stoneGroup,jungle,jungleImage,gameState,ground,groundImage,
 gameOver,gameOverImage,restart,restartImage,score ,iground ;
 
 
+//calling the function for loading the images
+function preload()
+{
+  //loading the animation for monkey
+  monkeyImage=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png") ;
+  
+  //loading the image for the stone
+  stoneImage=loadImage("stone.png");
+ 
+  //loading the image for banana
+  bananaImage=loadImage("Bananas.png");
+  
+  //loading the image for ground
+  groundImage=loadImage("ground.jpg");
 
-function preload(){
- monkeyImage=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png") ;
- stoneImage=loadImage("stone.png");
- bananaImage=loadImage("banana.png");
- //groundImage=loadImage("ground.jpg");
-jungleImage=loadImage("jungle.jpg");
-gameOverImage=loadImage("gameOver.png");
-restartImage=loadImage("restart.png");
+  //loading the image for the jungle
+  jungleImage=loadImage("jungle2.jpg");
+
+  //loading the image for gameOver
+  gameOverImage=loadImage("gameOver.png");
+
+  //loading the image for restart
+  restartImage=loadImage("restart.png");
 
 }
 
 
 function setup() {
-createCanvas(600,300);
-gamestate="play";
-monkey=createSprite(50,270,20,20);
-monkey.addAnimation("running",monkeyImage);
-monkey.scale=0.3;
-ground=createSprite(50,280,20,20);
-ground.addImage("moving",groundImage);
-ground.scale=0.3;
-jungle=createSprite(0,0,400,400);
-jungle.addImage("moving",jungleImage);
-stoneGroup=createGroup();
-foodGroup=createGroup();
-monkey.setCollider("rectangle",0,0,40,200);
-score=0;
-iground=createSprite(50,285,20,20);
-iground.visible=false;
-restart=createSprite(300,160);
-restart.addImage(restartImage);
-restart.scale=0.2;
-restart.visible=false;
-gameOver=createSprite(300,150);
-gameOver.addImage(gameOverImage);
-gameOver.scale=0.2;
-gameOver.visisble=false;
+  //creating the canvas
+  createCanvas(600,300);
+  ground.x=ground.width/2;
+  //assingnging a value to the gameState
+  gamestate="play";
+  
+  //creating the monkey sprite
+  monkey=createSprite(50,270,20,20);
+
+  //adding the animation for monkeyu
+  monkey.addAnimation("running",monkeyImage);
+
+  //scaling the monkwey
+  monkey.scale=0.3;
+
+  //creating the ground sprite
+  ground=createSprite(50,280,20,20);
+
+  //adding the image for ground
+  ground.addImage(groundImage);
+
+  //scaling the ground
+  ground.scale=0.3;
+
+  //creating the jungle sprite
+  jungle=createSprite(0,0,400,400);
+
+  //adding the image for jungle
+  jungle.addImage("moving",jungleImage);
+
+  //creating the stone group
+  stoneGroup=createGroup();
+  
+  //creating the food group
+  foodGroup=createGroup();
+  
+  //setting the collider radius for the monkey
+  monkey.setCollider("rectangle",0,0,40,200);
+  
+  //assigning the value to the score
+  score=0;
+  
+  //creating a sprite for the invisible ground
+  iground=createSprite(50,285,20,20);
+  
+  //making it invisible
+  iground.visible=false;
+  
+  //creating a sprite for restart
+  restart=createSprite(300,160);
+  
+  //adding the restart image
+  restart.addImage(restartImage);
+  
+  //scaling the restart sprite
+  restart.scale=0.2;
+ 
+  //making it invisible
+  restart.visible=false;
+ 
+  //creating the gameOver sprite
+  gameOver=createSprite(300,150);
+  
+  //adding the image for gameOver sprite
+  gameOver.addImage(gameOverImage);
+  
+  //scaling the gameOver sprite
+  gameOver.scale=0.2;
+ 
+  //making it invisible
+  gameOver.visisble=false;
 }
 
 
 function draw(){
- background(255); 
-if (gamestate==="play")
-{
+ 
+ 
+ 
+  //setting the background color
+  background(255); 
+  
+  //giving the ground a velocity
+  if (gamestate==="play")
+  {
   ground.velocityX=-6
+ 
+  //resetting the ground
   if(ground.x<0)
   {
     ground.x=ground.Width/2;
   }
-if(keyDown("space"&&monkey.y>240))
-{
+  
+  //controls for the monkey to jump when space is pressed
+  if(keyDown("space")&&monkey.y>280)
+  {
   monkey.velocityY=-10;
-}
-monkey.velocityY=monkey.velocityY+0.9;
-jungle.velocityX=-3;
-if(jungle.x<0)
-{
+  }
+
+  //adding gravity so that the monkey would come down after jumping
+  monkey.velocityY=monkey.velocityY+0.9;
+
+  //giving the jungle a velocity
+  jungle.velocityX=-3;
+
+  //resetting the jungle
+  if(jungle.x<0)
+  {
   jungle.x=jungle.Width/2;
-}
-Text("score "+score,540,40,textSize(20),fill("red"))
-switch(score)
+  }
+
+  
+
+  //increasing the size of the monkey at different values of the score
+  switch(score)
 {
-  case 10:  monkey.scale=0.3;
+  case 10:  monkey.scale=0.4;
             break;
-  case 20:  monkey.scale=0.4;
+  case 20:  monkey.scale=0.6;
             break;
-  case 30:  monkey.scale=0.5;
+  case 30:  monkey.scale=0.8;
             break; 
 
-default:    break;
-}
-spawnStones();
-spawnbananas();
-destroybananas();
-}
+  default:    break;
+  }
 
-monkey.collide(iground);
-if(stoneGroup.isTouching(monkey))
-{
-  monkey.scale=0.3;
-}
-if(stoneGroup.isTouching(monkey)&&monkey.scale===0.3)
-{
-  gameState="gameOver"
- 
-  stone.velocityX=0;
-  banana.velocityX=0;
-  jungle.velocityX=0;
+  //calling the function for spawning the stones
+  spawnStones();
   
-}
-if(gameState==="gameOver")
-{
-  score=0;
-  restart.visible=true;
-  gameOver.visible=true;
-}
-if(gameState==="gameOver"&& mousePressedOver(restart))
-{
-  score=0;
-  restart.visible=false;
-  gameOver.visible=false;
-  gameState="play";
-}
-
-
-drawSprites();
-}
-
-function spawnStones()
-{
-  if(World.frameCount%65===0)
-  {
-
-  stone=createSprite(400,280,20,20);
-  stone.addImage(stoneImage);
-  stone.scale=0.2;
-  stone.velocityX=-6;
-  stoneGroup.add(stone)
+  //calling the function for spawning the bananas
+  spawnbananas();
+  
+  //calling the function for destroying the bananas when eaten by the monkey
+  destroybananas();
   }
-}
 
-  function spawnbananas()
-{
-if(World.frameCount%75===0)
-{
-  rand=random(200,260)
-  bananas=createSprite(400,rand,20,20);
-  bananas.addImage(bananaImage);
-  bananas.scale=0.2;
-  bananas.velocityX=-6;
-  foodGroup.add(bananas);
-
-}
-} 
-
-function destroybananas()
-{
-  if(foodGroup.isTouching(monkey))
+  //colling the monkey with the invisible ground
+  monkey.collide(iground);
+  
+  //resetting the monkeySize if it is touching the stone
+  if(stoneGroup.isTouching(monkey))
   {
-    foodGroup.destroyEach();
-    score=score+2;
+    monkey.scale=0.3;
   }
+  
+  //changing the gamestate to gameOver if the stone group is touching monkey and monkey size is 0.3
+  if(stoneGroup.isTouching(monkey)&&monkey.scale===0.3)
+  {
+    gameState="Over"
+  stoneGroup.setVelocityX=0;
+  foodGroup.setVelocityX=0;
+  jungle.velocityX=0;
+    
+  }
+  
+  //displaying gameover and restart sprites when gameState is gameOver
+  if(gameState==="Over")
+  {
+    score=0;
+    restart.visible=true;
+    gameOver.visible=true;
+  }
+  
+  //restarting the game
+  if(gameState==="Over"&& mousePressedOver(restart))
+  {
+    
+    restart.visible=false;
+    gameOver.visible=false;
+    gameState="play";
+  }
+
+  //drawing the sprites
+  drawSprites();
+  //displaying the score
+  Text("score "+score,540,40,textSize(20),fill("red"))
 }
+
+  //function for spawning the stones after a specific interval of time
+  function spawnStones()
+  {
+    if(World.frameCount%65===0)
+    {
+
+    stone=createSprite(400,280,20,20);
+    stone.addImage(stoneImage);
+    stone.scale=0.2;
+    stone.velocityX=-6;
+    stoneGroup.add(stone)
+    stoneGroup.setlifetimeEach=100;
+  }
+  }
+
+    //function for spawning the bananas after specific interval of time
+    function spawnbananas()
+  {
+  if(World.frameCount%75===0)
+  {
+    rand=random(200,260)
+    bananas=createSprite(400,rand,20,20);
+    bananas.addImage(bananaImage);
+    bananas.scale=0.2;
+    bananas.velocityX=-6;
+    foodGroup.add(bananas);
+    banana.lifetime=100;
+  }
+  } 
+
+  //function for destroying the bananas when the monkey touches them
+  function destroybananas()
+  {
+    if(foodGroup.isTouching(monkey))
+    {
+      foodGroup.destroyEach();
+      score=score+2;
+    }
+  }
+ 
